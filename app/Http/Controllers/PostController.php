b35909e9990f6065b8250b9c218dd3d4f0c128ce
shiu358 <gemhne_12.1121@ezweb.ne.jp>
2021-09-22 22:06:17 +0900

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use app＼Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+    return view('index')->with(['post' => $post -> getPaginate()]);
+    }
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+}
