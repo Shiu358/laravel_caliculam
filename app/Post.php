77f6c7a5b97ba02191f7cdf55921116e5e6b7a46
shiu358 <gemhne_12.1121@ezweb.ne.jp>
2021-09-22 22:45:19 +0900

    second commit

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function index(Post $post)
+    {
+    return $post->get();
+    }
 }
